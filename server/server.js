87bce6a1b35cbf460d415c325a79561e921f866b
manash <mdihingia90@gmail.com>
2025-07-04 16:56:10 +0530

    Fix: Add frontend Render URL to CORS

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-import express from "express";
+/*import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
@@ -20,6 +20,46 @@ const allowedOrigins = [
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ origin: allowedOrigins, credentials: true }));
+
+// API Endpoints
+app.get("/", (req, res) => res.send("API Working"));
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
+
+app.listen(port, () => console.log(`Server started on PORT: ${port}`));
+*/
+
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import cookieParser from "cookie-parser";
+
+import connectDB from "./config/mongodb.js";
+import authRouter from "./routes/authRoutes.js";
+import userRouter from "./routes/userRoutes.js";
+
+const app = express();
+const port = process.env.PORT || 4000;
+connectDB();
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://auth-mern-client-p807.onrender.com"
+];
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+  origin: function(origin, callback){
+    if(!origin) return callback(null, true); // allow Postman, curl, etc.
+    if(allowedOrigins.indexOf(origin) === -1){
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true,
+}));
 
 // API Endpoints
 app.get("/", (req, res) => res.send("API Working"));
